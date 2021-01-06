235dd40b5370c70eec35b4fbf925bc9abb5af445
OBS <help.tgumbs1@gmail.com>
2021-01-06 14:11:56 -0500

    Create forEaach.js

---
+++ Javascript/forEaach.js
@@ -0,0 +1,25 @@
+/**
+ * Traditional JavaScript forEach loop with an added delay between iterations.
+ * @param {Array} array array to iterate over
+ * @param {Function} callbackfn The function to execute on each value
+ * @argument {any} callbackfn.value
+ * @argument {Number} callbackfn.index
+ * @param {Number} delay The time to wait between each iteration in ms
+ * @returns {Promise<This>}
+ */
+async function forEaach(array, callbackfn, delay) {
+    var arr = [...array];
+
+    setInterval(async () => {
+        if (!arr.length || !arr.length === 0) return clearInterval();
+
+        await callbackfn(arr[0], array.indexOf(arr[0]));
+
+        arr.shift();
+
+        if (!arr.length || !arr.length === 0) return clearInterval();
+
+    }, delay)
+
+}
+
